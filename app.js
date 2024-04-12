e25d59aefdbe677ef8f0d772482578cd8c7da9d3
josepeee <joseantonioplaza777@gmail.com>
2024-04-12 19:09:02 +0200

    conexcion a mongo

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const mongoose=require("mongoose")
 const PORT = 3000;
+require("dotenv").config();
+
 
 const app = express();
 app.get('/hola/:nombre', (req, res) => {
@@ -7,7 +10,28 @@ app.get('/hola/:nombre', (req, res) => {
     res.setHeader("Content-type", "text/html; chartset=utf-8");
     res.end(`<h2> hola mundo ${nombre}</h2>`);
 })
+
+const urlMongoose = process.env.DATABASE_URL_DEV;
+
+mongoose.connect(urlMongoose);
+
+const db = mongoose.connection;
+
+db.on("error", (error)=>{
+    console.error("Error al conectar");
+})
+
+db.once("connected", () =>{
+    console.log("Sucess conect");
+})
+
+db.on("disconected", () =>{
+    console.log("mongoose default connection is disconnected")
+})
+
 app.listen(PORT, () => {
     console.log(`Server running in http://localhost:${PORT}`);
 
 });
+
+// constraseña: 5wswpTUkuTQB3kZH
